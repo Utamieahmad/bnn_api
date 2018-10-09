ada33b0fa91fae8a28695b543121a557df9153d4
Ahmad <utamieahmad@gmail.com>
2018-10-09 16:09:53 +0700

    git set

--- app/Models/Berantas/KasusBrgBukti.php
+++ app/Models/Berantas/KasusBrgBukti.php
@@ -10,6 +10,8 @@ class KasusBrgBukti extends Authenticatable
     /* @author : Daniel Andi */
 
     use Notifiable;
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
     protected $primaryKey = 'kasus_barang_bukti_id';
     protected $table      = 'berantas_kasus_barang_bukti';
     public $timestamps    = false;
