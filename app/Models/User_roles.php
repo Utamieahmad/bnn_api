3b35ed9ca930ef031a039fe984b457ed6a4768dd
Ahmad <utamieahmad@gmail.com>
2018-10-09 23:40:34 +0700

    fixing soft delete

--- app/Models/User_roles.php
+++ app/Models/User_roles.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User_roles extends Model
 {
     /* @author : Daniel Andi */
+   use SoftDeletes;
+   protected $dates = ['deleted_at'];
     public $table       = "user_group";
     public $timestamps  = false;
     protected $hidden   = ['id'];
--- app/Models/rbac_users.php
+++ app/Models/rbac_users.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-//use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rbac_users extends Authenticatable
 {
     /* @author : Daniel Andi */
 
     use Notifiable;
-//    use SoftDeletes;
-//    protected $dates = ['deleted_at'];
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
     protected $primaryKey = 'user_id';
     public $timestamps    = false;
     protected $guarded    = ['user_id'];
