4a3e76503a2e233dffe928a55cd3c9f30c3cd543
dendry <dendryjava@gmail.com>
2018-10-09 18:03:49 +0700

    finally

--- app/Models/Group_menu.php
+++ app/Models/Group_menu.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+//use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Group_menu extends Model
 {
     /* @author : Daniel Andi */
-    use SoftDeletes;
-    protected $dates = ['deleted_at'];
+//    use SoftDeletes;
+//    protected $dates = ['deleted_at'];
     public $table       = "v_group_menu";
     public $timestamps  = false;
     protected $hidden   = ['id'];
--- app/Models/View_users.php
+++ app/Models/View_users.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\SoftDeletes;
+//use Illuminate\Database\Eloquent\SoftDeletes;
 
 class View_users extends Authenticatable
 {
     /* @author : Daniel Andi */
 
     use Notifiable;
-    use SoftDeletes;
-    protected $dates = ['deleted_at'];
+//    use SoftDeletes;
+//    protected $dates = ['deleted_at'];
     protected $primaryKey = 'user_id';
     protected $table      = 'v_rbac_users_groups';
     public $timestamps    = false;
