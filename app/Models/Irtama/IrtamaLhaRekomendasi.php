0d5384b65adaaad8375b2a9ee31271655b2ab35b
dendry <dendryjava@gmail.com>
2018-10-09 17:04:36 +0700

    set module soft delete module irtama

--- app/Models/Irtama/IrtamaLhaRekomendasi.php
+++ app/Models/Irtama/IrtamaLhaRekomendasi.php
@@ -4,6 +4,7 @@ namespace App\Models\Irtama;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class IrtamaLhaRekomendasi extends Authenticatable
 {
