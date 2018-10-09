4e075ed3aebd1ee1cb00e6a087fb5040af6c3c34
dendry <dendryjava@gmail.com>
2018-10-09 17:00:14 +0700

    set module soft delete module rehab

--- app/Models/Rehab/InfoUmumLembaga.php
+++ app/Models/Rehab/InfoUmumLembaga.php
@@ -4,6 +4,7 @@ namespace App\Models\Rehab;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InfoUmumLembaga extends Authenticatable
 {
